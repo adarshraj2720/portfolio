7b951d7e2488965abf76b063191bcea5cd5d3e69
Adarsh Raj <adarshraj2721@gmail.com>
2022-12-15 16:23:54 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Skill from "./components/Skill";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Social from "./components/Social";
+
 import About from "./components/About";
 
 function App() {
@@ -21,9 +21,7 @@ function App() {
         <Projects />
         <Contact />
       </div>
-      <div className="pl-4 pr-4  sm:pl-16 sm:pr-16 ">
-        <Social />
-      </div>
+
     </div>
   );
 }
